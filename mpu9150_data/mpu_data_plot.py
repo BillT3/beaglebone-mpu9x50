36e23624ebe7b8b5e45a01b2dc4e463e793714c3
Matthew Vernacchia <mvernacc@mit.edu>
2015-12-04 01:56:33 -0500

    Add argparse to mpu data plot

--- mpu9150_data/mpu_data_plot.py
+++ mpu9150_data/mpu_data_plot.py
@@ -1,29 +1,37 @@
 import cPickle as pickle
 import numpy as np
 from matplotlib import pyplot as plt
+import argparse
+
+def main(args):
+    with open(args.data_file, 'rb') as f:
+        d = pickle.load(f)
+
+    colors = ['r', 'g', 'b']
+
+    plt.subplot(3,1,1)
+    for i in xrange(3):
+        plt.plot(d['time'], d['accel_data'][:,i], color=colors[i])
+    plt.xlabel('Time [s]')
+    plt.ylabel('Acceleration [g]')
+
+    plt.subplot(3,1,2)
+    for i in xrange(3):
+        plt.plot(d['time'], d['gyro_data'][:,i], color=colors[i])
+    plt.xlabel('Time [s]')
+    plt.ylabel('Ang rate [deg / s]')
+
+    plt.subplot(3,1,3)
+    for i in xrange(3):
+        plt.plot(d['time'], d['mag_data'][:,i], color=colors[i])
+    plt.xlabel('Time [s]')
+    plt.ylabel('Magnetic field [uT]')
+
+    plt.show()
 
 
-with open('mpu_data_2015-12-02_06-23-06.p', 'rb') as f:
-    d = pickle.load(f)
-
-colors = ['r', 'g', 'b']
-
-plt.subplot(3,1,1)
-for i in xrange(3):
-    plt.plot(d['time'], d['accel_data'][:,i], color=colors[i])
-plt.xlabel('Time [s]')
-plt.ylabel('Acceleration [g]')
-
-plt.subplot(3,1,2)
-for i in xrange(3):
-    plt.plot(d['time'], d['gyro_data'][:,i], color=colors[i])
-plt.xlabel('Time [s]')
-plt.ylabel('Ang rate [deg / s]')
-
-plt.subplot(3,1,3)
-for i in xrange(3):
-    plt.plot(d['time'], d['mag_data'][:,i], color=colors[i])
-plt.xlabel('Time [s]')
-plt.ylabel('Magnetic field [uT]')
-
-plt.show()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Plot pickled MPU data.')
+    parser.add_argument('-d', '--data_file', type=str, help='Name of pickle file.')
+    args = parser.parse_args()
+    main(args)
